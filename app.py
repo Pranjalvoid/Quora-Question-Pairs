4f1c95135b21e73e89520dc8c4955d2291774d53
Pranjalvoid <168279304+Pranjalvoid@users.noreply.github.com>
2025-12-09 16:31:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
 import streamlit as st
 import helper
 import pickle
-
-model = pickle.load(open('model.pkl','rb'))
+import urllib.request
 
 st.header('Duplicate Question Pairs')
+
+# ---------- Load Model from HuggingFace ----------
+MODEL_URL = "https://huggingface.co/pranjalvoid/duplicate-question-detector-model/resolve/main/model.pkl"
+model = pickle.load(urllib.request.urlopen(MODEL_URL))
+# -------------------------------------------------
 
 q1 = st.text_input('Enter question 1')
 q2 = st.text_input('Enter question 2')
 
 if st.button('Find'):
-    query = helper.query_point_creator(q1,q2)
+    query = helper.query_point_creator(q1, q2)
     result = model.predict(query)[0]
 
     if result:
         st.header('Duplicate')
     else:
         st.header('Not Duplicate')
-
-
